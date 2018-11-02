bb8ba1081f2473b6b4be3033d141f7c27d550ba5
Olubunmi Odeyinka <olubunmi.odeyinka@aurea.com>
2018-11-02 15:21:03 +0200

    Connect login to Server

--- atc_web/src/components/modules/home/LoginPage.js
+++ atc_web/src/components/modules/home/LoginPage.js
@@ -1,88 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {Field ,Form} from 'formik';
-import {withFormik} from 'formik';
+import {Field ,Form, withFormik} from 'formik';
 import Yup from 'yup';
 import {bindActionCreators} from 'redux';
 import * as loginAction from '../../../actions/homeActions';
-import * as operation from '../../../config/constants/operationTypes';
-//import {genresFormattedForDropdown} from '../../common/utilities/selectors';
 import toastr from 'toastr';
 import _ from 'underscore';
 
 
 export class LoginPage extends React.Component {
 
-    state = {
-        formRootObjectName: 'login',
+    user = {username: '', password: ''};
 
-        themeClasses: ' bg-white',
-        showSavingButton: true,
-        savingString:['Logging In', 'Login', 'btn-success'],
-        formState: {}
-    };
-
-    // headerGenerator = (headerString) => {
-    //     return (
-    //         <div className='py-2'>
-    //             <h4>{headerString}</h4>
-    //             <hr/>
-    //         </div>);
-    // }
-
-    cardDisplayConfig ={
-        hideBackToList: true,
-        //cardClass:
-    }
-
-    fieldsDefinition = {
-        Email: {
-            type: 'text',
-            label: 'Email',
-            defaultValue: ''
-        },
-        Password: {
-            type: 'password',
-            label: 'Password',
-            defaultValue:''
-        }
-    }
-
-    componentDidMount =() =>{
-        if (this.props.match.params.id) {
-            this.props.actions.getLoginById(this.props.match.params.id);
-        }
-
-        if (this.props.match.params.operator) {
-
-            this.setState(() => ({urlOpeartion : this.props.match.params.operator}));
-
-            switch(this.props.match.params.operator){
-                case operation.Create_URL_String:
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        // this.props.lookUpsAction.loadNeedClasses();
-        // this.props.lookUpsAction.loadDepartments();
-        // this.props.lookUpsAction.loadNeedDocTypes();
-    }
-
-    redirect =(successMessage) => {
-        toastr.success(successMessage);
-        this.props.history.push('/dashboard');
-    }
-
-    FormDom = props =>{
-        this.parentState={...props.values};
-        //console.log(this.parentState);
-
-        //setValues
-        return (
-            <section id="login" className="p-4">
+    theForm = ({values,errors})=>(  
+        <section id="login" className="p-4">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-6 offset-md-3">
@@ -93,7 +25,7 @@ export class LoginPage extends React.Component {
                                 <div className="card-body">
                                     <Form>
                                       <div className='form-group'>
-                                         <label for='username' htmlFor='username'>UserName</label>
+                                         <label htmlFor='username'>UserName</label>
                                             <Field
                                                 id='username'
                                                 name='username'
@@ -101,91 +33,62 @@ export class LoginPage extends React.Component {
                                                 type='text'
                                                 placeholder='type user name here'
                                                 />
-                                        </div>
-                                        <div className='form-group'>
-                                        <label for='password' htmlFor='password'>Password</label>
+                                       
+                                        <label htmlFor='password'>Password</label>
                                             <Field
                                                 id='password'
                                                 name='password'
                                                 className='form-control'
                                                 type='password'
-                                                placeholder='type user name here'
+                                                placeholder='type your password'
                                                 />
                                         </div>
-
                                         <input
                                             type="submit"
-                                            // disabled={this.props.isSubmitting}
-                                            // value={this.props.isSubmitting ? this.props.formState.savingString[0] + '...' : this.props.formState.savingString[1]}
-                                            className={"d-print-none btn btn-block offset-2 col-8 " +  "btn-info"}//this.props.formState.savingString[2] ||
+                                            className={"d-print-none btn btn-block offset-2 col-8 btn-info"}//this.props.formState.savingString[2] ||
                                             onClick={this.props.onSave}/>
                                     </Form>
                                 </div>
                                 <div className="card-footer bg-dark"></div>
                             </div>
-                            {/* <GeneralForm
-                                formState={{...this.state}}
-                                //lookUps={{'DepartmentID':this.props.departments, 'NeedClass': this.props.needclasses}}
-                                fields={this.fieldsDefinition}
-                                operation={this.props.match.params.operator}
-                                cardDisplayConfig={{...this.cardDisplayConfig}}
-                                //thisRef={this}
-                                {...props}
-                            /> */}
-                            
                         </div>
                     </div>
                 </div>
             </section>
-        );}
+    )
 
-    theEditableForm = withFormik({
-        mapPropsToValues({login}){
+    formikApp = withFormik({
+        mapPropsToValues(user){
             return{
-                ...login
+                usernane:'',
+                password:''
             }
         },
         validationSchema: Yup.object().shape({
-            Email: Yup.string('Email is a string').email('This is not a valid Email').required('Email field is required'),
-            Password: Yup.string('Password is a string').required('Password field is required')
+            username: Yup.string('User Name is a string').required('User Name field is required'),
+            password: Yup.string('Password is a string').required('Password field is required')
         }),
         handleSubmit(values, bag){
-            bag.setSubmitting(true);
-            let subMissionPromise = null;
             let successMessage = null;
-            switch(bag.props.urlOpeartion){
-                default:
-                    subMissionPromise = bag.props.loginAction.login(values);
-                    successMessage = 'Login Successful'
-                    break;
-            }
-
-            subMissionPromise
-                .then(() => bag.props.redirect(successMessage))
-                .catch(error => {
-                    bag.setSubmitting(false);
-                    toastr.error(error);
-                });
-        },
-        enableReinitialize: true
-    })(this.FormDom)
+            bag.props.loginAction.login(values)
+            .then(() => bag.props.redirect(successMessage))
+                 .catch(error => {
+                 bag.setSubmitting(false);
+                     toastr.error(error);
+                 });
+        }
+    })(this.theForm)
 
     render =()=> {
-        return  <this.theEditableForm
-                login={this.props.login}
-                loginAction={this.props.loginAction}
-                urlOpeartion={this.state.urlOpeartion}
-                redirect={this.redirect}
-            />
+        return  <this.formikApp
+        login={this.props.login}
+        loginAction={this.props.loginAction}
+        redirect={this.redirect}
+        />
     }
 }
 
-LoginPage.propTypes = {
-    login: PropTypes.object.isRequired,
-    //genres: PropTypes.array.isRequired,
-    lookUpsAction: PropTypes.object.isRequired,
-    actions: PropTypes.object.isRequired
-};
+LoginPage.propTypes = {};
 
 //Pull in the React Router context so router is available on this.context.router.
 LoginPage.contextTypes = {
@@ -200,7 +103,6 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        //lookUpsAction: bindActionCreators(lookUpsAction, dispatch),
         loginAction: bindActionCreators(loginAction, dispatch),
     };
 }
