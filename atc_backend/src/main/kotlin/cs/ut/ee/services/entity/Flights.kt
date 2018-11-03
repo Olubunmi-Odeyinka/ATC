669411fb8db14897a66eeeed990bd280e610d1ab
Zukkari <stanislav.myshkovski@gmail.com>
2018-11-03 12:35:25 +0200

    more pojos

---
+++ atc_backend/src/main/kotlin/cs/ut/ee/services/entity/Flights.kt
@@ -0,0 +1,27 @@
+package cs.ut.ee.services.entity
+
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
+
+object Flights : IntIdTable() {
+    val name = varchar("name", 50)
+    val plane = varchar("plane", 50)
+    val company = varchar("company", 50)
+    val lane = integer("lane")
+    val time = datetime("time")
+    val timeTable = reference("timetable", TimeTables, onDelete = ReferenceOption.CASCADE)
+}
+
+class Flight(id: EntityID<Int>) : IntEntity(id) {
+    var name by Flights.name
+    var plane by Flights.plane
+    var company by Flights.company
+    var lane by Flights.lane
+    var time by Flights.time
+    var timetable by Flights.timeTable
+
+    companion object : IntEntityClass<Flight>(Flights)
+}
---
+++ atc_backend/src/test/kotlin/cs/ut/ee/services/TimetablesTest.kt
@@ -0,0 +1,5 @@
+package cs.ut.ee.services
+
+class TimetablesTest {
+
+}
